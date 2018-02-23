762997815e848db53b8803533debfc0b162b9608
Mozgor <mozgor@laposte.net>
2018-02-23 19:46:11 +0100

    feat: delete player + update settings scene (input)

--- ContrarioUnity/Assets/Scripts/Models/PlayerPrefManager.cs
+++ ContrarioUnity/Assets/Scripts/Models/PlayerPrefManager.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 namespace AssemblyCSharp
 {
 	public class PlayerPrefManager {
-		public static int nbPlayer = 0;
+		public static int nbPlayer = 2;
 		public static List<Player> playerList = new List<Player>();
+		public static int nbCards = nbPlayer * 5;
 	}
 }
